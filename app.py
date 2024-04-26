0304f959a1199233655ad91f4c0874e6c022a766
trpnayak <60289278+trpnayak@users.noreply.github.com>
2024-04-26 14:21:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+#this is a sample flask application
+
+from flask import Flask
+app= Flask(__name__)
+@app.route("/")
+def hello():
+    return "<h2>HEllo World - Docker Flask Deployment Lab</h2><hr/>"
+app.run(host="0.0.0.0",port=5000)    
