b68f5e54d83baf5284b33cf5a0be18cd6c6ed202
trpnayak <60289278+trpnayak@users.noreply.github.com>
2024-05-14 21:18:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app= Flask(__name__)
 @app.route("/")
 def hello():
     return "<h2>CI/CD hhh125 </h2><hr/>"
-app.run(debug=True,host="192.168.43.154",port=3000)    
+app.run(debug=True,host="0.0.0.0",port=3000)    
